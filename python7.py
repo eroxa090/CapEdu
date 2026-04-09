3c15377a3e32e7f1852ad107c26fc895688b9c4d
eroxa090 <ersultan1312@icloud.com>
2026-04-10 02:28:56 +0500

    Add files via upload

---
+++ python7.py
@@ -0,0 +1,13 @@
+prog = ("программирование - это круто!")
+for i in range (7):
+    print (prog)
+
+
+for i in range(3):
+    prog = input("ваш любимый цвет")
+print ("заказ принят")
+
+
+number = (10,20,30,40,50)
+for num in number:
+    print(num)
