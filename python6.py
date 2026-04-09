4207c2e560c2ed56b863ef925e6b6e1e762e73ee
eroxa090 <ersultan1312@icloud.com>
2026-04-10 02:28:41 +0500

    Add files via upload

---
+++ python6.py
@@ -0,0 +1,16 @@
+age = int(input("your age:"))
+if age >= 18:
+    print("you can get licens")
+else :
+    print("you cann not")
+
+    
+your_apples = int(input( "skolko yablok u vas?"))
+apples_friend = int(input("skolko u druga?"))
+if your_apples > apples_friend :
+    print("u vas bolshe")
+elif your_apples < apples_friend :
+    print("u druga bolshee")
+else:
+    print("odinakovo")
+
